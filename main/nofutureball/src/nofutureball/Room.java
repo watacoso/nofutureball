76b80f8b7176966ec8ddb8768e51c1350697dbb0
watacoso <watacoso@gmail.com>
2013-06-24 22:47:49 +0200

    deleted:    .settings/org.eclipse.jdt.core.prefs 	modified:   src/nofutureball/Room.java

--- main/nofutureball/src/nofutureball/Room.java
+++ main/nofutureball/src/nofutureball/Room.java
@@ -1,5 +1,10 @@
 package nofutureball;
 
-public class Room {
+public class Room extends Entity{
+
+	public Room(float x, float y, float width, float height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
 
 }
