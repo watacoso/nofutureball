c8c8f53e4e9b00aa6a7f93931ee3f1a4587145bd
watacoso <watacoso@gmail.com>
2013-07-06 12:11:03 +0200

    Walls now render the right way, leaving an empty space in presence 	of a door. added Level class. it has the tools to add new rooms , 	and the task to procedurally generate the map. (Map generation to 	be implemented)

--- main/nofutureball/src/nofutureball/Game.java
+++ main/nofutureball/src/nofutureball/Game.java
@@ -14,11 +14,9 @@ public class Game extends BasicGame {
 	public Container entities = null;
 	public Container ui = null;
 	
-	private Room r1;
-	private Room r2;
-	private Room r3;
-	private Room r4;
-	private Room r5;
+	public Level level;
+	
+
 	private Camera cam = null;
 	
 	
@@ -34,38 +32,21 @@ public class Game extends BasicGame {
 		
 	}
 
-	
-
 	@Override
 	public void init(GameContainer gc) throws SlickException {
 
-		r1 = new Room(0,-120, 10, 10);
-		r2 = new Room(615, 0, 10, 10);
-		r3 = new Room(1230, 60, 10, 10);
-		r4 = new Room(495, 315, 10, 10);
-		r5 = new Room(615, 630, 10, 10);
-		
-		r1.addWalls(entities);
-		r2.addWalls(entities);
-		r3.addWalls(entities);
-		r4.addWalls(entities);
-		r5.addWalls(entities);
-		
-		mapContainer.add(r2);
-		mapContainer.add(r1);
-		mapContainer.add(r3);
-		mapContainer.add(r4);
-		mapContainer.add(r5);
-		
+
+		level=new Level(entities,mapContainer);
+		level.generateMap();
 		cam = new Camera(gameContainer);
 
 		AnimationSource.init();
 		NoFutureBall.setGameContainer(gc);
 		ArrayList<Player> players = new ArrayList<Player>();
-		players.add(new Player(r1,100, 40, KeySet.ONE));
-		players.add(new Player(r4, 40, 40, KeySet.TWO));
-		players.add(new Player(r2, 100, 100, KeySet.THREE));
-		players.add(new Player(r3, 0, 100, KeySet.FOUR));
+		players.add(new Player(level.getStartRoom(),100, 40, KeySet.ONE));
+		players.add(new Player(level.getStartRoom(), 200, 40, KeySet.TWO));
+		//players.add(new Player(r2, 100, 100, KeySet.THREE));
+		//players.add(new Player(r3, 0, 100, KeySet.FOUR));
 		startNewGame(players);
 		SoundManager.load();
 		SoundManager.mixedSound("Pickup");
--- main/nofutureball/src/nofutureball/Room.java
+++ main/nofutureball/src/nofutureball/Room.java
@@ -1,5 +1,8 @@
 package nofutureball;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
@@ -9,12 +12,15 @@ import org.newdawn.slick.geom.Vector2f;
 public class Room extends Entity {
 
 	public Boolean occupied;
-
+	public ArrayList<Door> doors;
 	private Graphics g = new Graphics();
 	Image floor;
-
+	
+	public static int wallSpessor=15;
+	public static int tileWidth=60;
+	public static int tileHeight=30;
 	public Room(float x, float y, int width, int height) {
-		super(x, y, width * 60, height * 30);
+		super(x, y, width * tileWidth, height * tileHeight);
 		this.width = width;
 		this.height = height;
 		try {
@@ -23,7 +29,22 @@ public class Room extends Entity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		doors=new ArrayList<Door>();
 	}
+	
+	public Room(int width, int height) {
+		super(0, 0, width * tileWidth, height * tileHeight);
+		this.width = width;
+		this.height = height;
+		try {
+			floor = new Image("assets/sprites/floorTiles.png");
+		} catch (SlickException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		doors=new ArrayList<Door>();
+	}
+	
 	/** Number of tiles on the x-axis */
 	private int width = 0;
 	/** Number of tiles on the y-axis */
@@ -35,13 +56,18 @@ public class Room extends Entity {
 
 	public void render(Camera cam) {
 		Vector2f screenPos = getScreenPos(cam);
-		g.setColor(Color.decode("#565C76"));
+		
 		
 		// Floor
 		Image scaledFloorTile = floor.getScaledCopy(cam.getZoom());
 		// just caching values to limit calculations
-		float floorWidth = 60 * cam.getZoom();
-		float floorHeight = 30 * cam.getZoom();
+		float floorWidth = tileWidth * cam.getZoom();
+		float floorHeight = tileHeight * cam.getZoom();
+		float _wallSpessor = wallSpessor*cam.getZoom();
+		g.setColor(Color.decode("#585D78"));
+		g.setLineWidth(_wallSpessor);
+		g.drawRect(screenPos.x, screenPos.y, floorWidth*width, floorHeight*height);
+		g.setColor(Color.decode("#565C76"));
 		g.fillRect(screenPos.x, screenPos.y, floorWidth*width, floorHeight*height);
 		for (float y = 0, i = 0; i < height; y += floorHeight, i ++) {
 			for (float x = 0, j = 0; j < width; x += floorWidth, j ++) {
@@ -52,14 +78,44 @@ public class Room extends Entity {
 				//floor.draw(screenPos.x + x, screenPos.y + y, floorWidth, floorHeight);
 			}
 		}
+		
 
 	}
 
 	public void addWalls(Container target) {
-		target.add(new Wall(this,position.x, position.y, 1, size.x));
-		target.add(new Wall(this,position.x - 15, position.y, 2, size.y));
-		target.add(new Wall(this,position.x + size.x, position.y, 2, size.y));
-		target.add(new Wall(this,position.x, position.y + size.y + 15, 1, size.x));
+		Collections.sort(doors);
+		float LY=0,RY=0,TX=0,BX=0;
+		for(int i=0;i<doors.size();i++){
+			Door r=doors.get(i);
+			
+			if(r.side=="top"||r.side=="bottom"){
+				if(position.y==r.position.y+15){
+					System.out.println("top");
+					target.add(new Wall(this,position.x+TX, position.y, 1, r.position.x-position.x+TX));
+					TX=r.position.x-position.x+r.size.x;
+				}
+				else{
+					target.add(new Wall(this,position.x+BX, position.y + size.y + wallSpessor, 1, r.position.x-position.x+BX));
+					BX=r.position.x-position.x+r.size.x;
+				}
+			}
+			else{
+				if(position.x==r.position.x+15){
+					target.add(new Wall(this,position.x - wallSpessor, position.y+LY, 2, r.position.y-position.y+LY-15));
+					LY=r.position.y-position.y+r.size.y;
+				}
+				else{
+					target.add(new Wall(this,position.x + size.x, position.y+RY, 2, r.position.y-position.y+RY-15));
+					RY=r.position.y-position.y+r.size.y;
+				}
+			}
+
+		}
+		
+		target.add(new Wall(this,position.x+TX, position.y, 1, size.x-TX));
+		target.add(new Wall(this,position.x - wallSpessor, position.y+LY, 2, size.y-LY));
+		target.add(new Wall(this,position.x + size.x, position.y+RY, 2, size.y-RY));
+		target.add(new Wall(this,position.x+BX, position.y + size.y + wallSpessor, 1, size.x-BX));
 
 	}
 	
